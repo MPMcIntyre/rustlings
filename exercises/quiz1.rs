8a576cda09deb0fc02d49da8b2a49605fd67c2c5
Michael McIntyre <mpmcintyre@yahoo.com>
2024-03-25 03:56:53 +0100

    Finished move semantics

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i32) -> i32 {
+    if number > 40 {
+        return number;
+    } else {
+        return number * 2;
+    }
+}
 
 // Don't modify this function!
 #[test]
